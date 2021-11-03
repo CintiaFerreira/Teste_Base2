e51e1b0294e8b002ad71067cc9207f0c453fe6db
Cintia Ferreira <cintiaferreiradiv@gmail.com>
2021-11-02 23:03:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteBase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
